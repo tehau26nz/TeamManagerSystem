2c0e27f104d8f282edfe49dc97e31a4ac79c5640
shingteha <Tahiti2021>
2021-10-31 02:56:58 +1300

    First commit with simple UI for the app

---
+++ TeamManagerSystem/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("UI_Intro.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Image icon = new Image("football_color_icon.png");
+			primaryStage.getIcons().add(icon);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ TeamManagerSystem/src/module-info.java
@@ -0,0 +1,6 @@
+module TeamManagerSystem {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
