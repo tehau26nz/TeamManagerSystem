deba937b5208409039e3ee4322af642247bb45f7
shingteha <Tahiti2021>
2021-11-01 10:59:07 +1300

    Made modifications

--- TeamManagerSystem/src/application/SceneController.java
+++ TeamManagerSystem/src/application/SceneController.java
@@ -54,5 +54,13 @@ public class SceneController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void switchToTeams(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Teams.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
