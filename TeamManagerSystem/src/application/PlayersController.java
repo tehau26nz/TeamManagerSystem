ca05a13eba24ca04567b1f3c97b35d24e55d33ac
Selina <yutong@myvuw.ac.nz>
2021-11-01 20:55:02 +1300

    Add back buttons to statistics and player layouts.

--- TeamManagerSystem/src/application/PlayersController.java
+++ TeamManagerSystem/src/application/PlayersController.java
@@ -1,17 +1,24 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import models.Player;
 import models.Team;
 
@@ -28,7 +35,7 @@ public class PlayersController  implements Initializable {
 	@FXML private TextField addPlayerAllPositions;
 	@FXML private Button btnAddPlayer;
 	@FXML private Button btnDeletePlayer;
-	
+	@FXML private ImageView ivPlayerPrevious;
 	
 	private ObservableList<Player> players = FXCollections.observableArrayList();
 	
@@ -51,6 +58,14 @@ public class PlayersController  implements Initializable {
 		addPlayerBirthplace.clear();
 		addPlayerHeight.clear();
 		addPlayerAllPositions.clear();
+	}
+	
+	@FXML public void handleImageViewAction(Event e) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/layouts/Dashboard.fxml"));
+		Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 	@FXML public void deleteRowFromTableViewAction(Event e) {
