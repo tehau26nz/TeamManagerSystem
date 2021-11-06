b8cd1d29e21e6b31b4a7d7b30ef4b08f9216d0ce
Selina <yutong@myvuw.ac.nz>
2021-11-06 22:48:29 +1300

    Add documentation to DataHelper class

--- TeamManagerSystem/src/application/DataHelper.java
+++ TeamManagerSystem/src/application/DataHelper.java
@@ -3,43 +3,51 @@ package application;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
 import models.Player;
 import models.Team;
-
-
-//nz.ac.wgtn.teammanager.ui
-//nz.ac.wgtn.teammanager.models
-//nz.ac.wgtn.teammanager.controllers
-//nz.ac.wgtn.teammanager.tests
-
 /**
  * This class is designed to work with data.
- * Currently it is only used to load data from text file.
- * @author Selina
- * @version 1.0
+ * Currently it is only used to load data into model from text file.
+ * In the future it could be used to connect to database.
+ * @author Selina Yu
  * @since 2021-11-01
  */
 public class DataHelper {
+	/**
+	 * List of team objects
+	 */
 	private ArrayList<Team> teams = new ArrayList<Team>();
+	/**
+	 * List of team names
+	 */
 	private ArrayList<String> teamnames = new ArrayList<String>();
-	
+	/**
+	 * Constructor - create a new DataHelper object with given data source file
+	 * @param fileName
+	 */
 	public DataHelper(String fileName) {
 		setTeams(fileName);
 		setTeamnames();
 	}
-
+	/**
+     * Accessor - method to get list of teams
+     * @return list of teams
+     */
 	public ArrayList<Team> getTeams() {
 		return teams;
 	}
-
-	/*load teams from  the teams.txt file*/
+	/**
+	 * Mutator - method to set list of teams with loading team data from text file
+	 * @param file name
+	 */
 	public void setTeams(String fileName) {
 		this.loadFile(fileName);
 	}
-	
+	/**
+	 * load team and player data from  the teams.txt file
+	 * @param fileName
+	 */
 	public void loadFile(String filename) {
 		try {
 			Scanner scan = new Scanner(new File(filename));
@@ -71,17 +79,27 @@ public class DataHelper {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+     * Accessor - method to get list of team names
+     * @return list of team names
+     */
 	public ArrayList<String> getTeamnames() {
 		return teamnames;
 	}
-
+	/**
+	 * Mutator - method to set list of teams names
+	 * @param list of team names
+	 */
 	public void setTeamnames() {
 		for (Team t : teams) {
 			teamnames.add(t.getTeamName());
 		}
 	}
-	
+	/**
+	 * method to get team object by given team name string
+	 * @param teamname
+	 * @return team object
+	 */
 	public Team getTeambyName(String teamname) {
 		for (Team t : teams) {
 			if(t.getTeamName().equals(teamname)) {
